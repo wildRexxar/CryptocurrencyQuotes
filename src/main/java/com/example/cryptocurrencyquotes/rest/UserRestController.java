93c5b5edb91133beb7dfae9b7e83f6cde9cb4781
Ilya <ilya.skavysh@gmail.com>
2022-02-28 21:27:20 +0300

    add target

--- src/main/java/com/example/cryptocurrencyquotes/rest/UserRestController.java
+++ src/main/java/com/example/cryptocurrencyquotes/rest/UserRestController.java
@@ -1,38 +1,27 @@
 package com.example.cryptocurrencyquotes.rest;
 
 import com.example.cryptocurrencyquotes.dto.CryptocurrencyUserDto;
-import com.example.cryptocurrencyquotes.entity.Cryptocurrency;
-import com.example.cryptocurrencyquotes.mapped.CryptocurrencyUserMapper;
-import com.example.cryptocurrencyquotes.mapped.UserMapper;
 import com.example.cryptocurrencyquotes.service.UserService;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/user")
 public class UserRestController {
 
-    private final UserService userService;
-    private final UserMapper userMapper;
-    private final CryptocurrencyUserMapper cryptoCurrencyUserMapper;
+    @Autowired
+    private UserService userService;
 
-    public UserRestController(UserService userService, UserMapper userMapper, CryptocurrencyUserMapper cryptoCurrencyUserMapper) {
-        this.userService = userService;
-        this.userMapper = userMapper;
-        this.cryptoCurrencyUserMapper = cryptoCurrencyUserMapper;
-    }
 
-    @PostMapping("/api/user/registration")
-    public ResponseEntity<CryptocurrencyUserDto> notify(@RequestBody CryptocurrencyUserDto cryptocurrencyUserDto) throws JsonProcessingException {
-        if (userService.findByUsername(cryptocurrencyUserDto.getUserDto().getUsername())) {
+    @PostMapping("/registration")
+    public ResponseEntity<CryptocurrencyUserDto> notify(String username) {
+        if(userService.findByUsername(username){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        Cryptocurrency cryptocurrency = cryptoCurrencyUserMapper.cryptocurrency(cryptocurrencyUserDto);
-        cryptocurrency.setUser(userMapper.user(cryptocurrencyUserDto.getUserDto()));
-        userService.registration(cryptocurrency);
-        return new ResponseEntity<>(cryptocurrencyUserDto, HttpStatus.CREATED);
+            userService.registration(username);
     }
 }
---
+++ src/test/java/com/example/cryptocurrencyquotes/CryptocurrencyQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptocurrencyquotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptocurrencyQuotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
